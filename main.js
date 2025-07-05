c5e90e1b853fbe4087acddc1f31c2ae4fd97dc03
Sayyid Abbas <sayyid.abbas.ar@gmail.com>
2025-07-05 17:15:00 +0300

    Fixing The Problem In JS With The Other Links

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ let otherLinks = document.getElementById("otherLinks");
 other.onclick = function () {
     otherLinks.classList.toggle("show");
 }
+document.addEventListener("click", function (e) {
+    if(!other.contains(e.target) && !otherLinks.contains(e.target)) {
+        otherLinks.classList.remove("show");
+    }
+});
