76ef885738e0ac2ae78bfab341fe332bf57f64be
Sayyid Abbas <sayyid.abbas.ar@gmail.com>
2025-07-08 09:33:52 +0300

    Fixing a small problem with the click outside the links

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 // For the other links to show on click
 
+//Add remove the show class every time a click on the links is happening
 let other = document.getElementById("other");
 let otherLinks = document.getElementById("otherLinks");
 other.onclick = function () {
     otherLinks.classList.toggle("show");
 }
 
+// To remove the show class when clicking outside the links
+document.addEventListener("click", function (e) {
+    if(!other.contains(e.target) && !otherLinks.contains(e.target)) {
+        otherLinks.classList.remove("show");
+    }
+});
+
 // A button to scroll up
 
 let myButton = document.querySelector(".btn");
+
+// Showing the button when hitting a certain number of pixels 
 window.onscroll = function() {
     if(window.scrollY < 1000) {
         myButton.classList.remove("show");
@@ -16,5 +26,6 @@ window.onscroll = function() {
         myButton.classList.add("show");
     }
 }
-myButton.onclick = () => window.scrollTo({top: 0, behavior: "smooth"});
 
+// Scrolling up on click
+myButton.onclick = () => window.scrollTo({top: 0, behavior: "smooth"});
